2a0f941484c4a5cdf44eb26fb6ac88f58bfe74ad
Ujjal Bhowmik <ubhowmik@illinois.edu>
2025-02-20 14:06:05 -0600

    lec10c

--- lec10/str_test1.c
+++ lec10/str_test1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #define SIZE 20
 int main(){
-    char input[SIZE];
+    char input[SIZE]="abc1 34 21";
     int var1 = 0, var2=0;
     char des[SIZE];
 
-    fgets(input, SIZE, stdin);
-    int rc  =  sscanf(input, "%d%d%s", &var1, &var2, des);
+    //fgets(input, SIZE, stdin);
+    int rc  =  sscanf(input, "%s%d%d", des, &var1, &var2);
     printf("%s\n", input);
     printf("var1: %d\n", var1);
     printf("vars2: %d\n", var2);
