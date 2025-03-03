38d5c4a12d6a4265eb94f73a86cc32ec8093a082
ubhowmik <46545523+ubhowmik@users.noreply.github.com>
2025-03-03 08:23:22 -0600

    Update quick_sort.c

--- lec11/quick_sort.c
+++ lec11/quick_sort.c
@@ -1,86 +1,94 @@
-#include<stdio.h>
-int partition(int array[], int l, int h);
-void quickSort(int array[], int l, int h);
-void swap(int *a, int *b);
-void print(int array[], int size);
+/******************************************************************************
 
-int partition(int array[], int l, int h)
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+// An iterative implementation of quick sort
+#include <stdio.h>
+
+// A utility function to swap two elements
+void swap(int* a, int* b)
 {
-int i=l-1;
-int x=array[h];
-int j;
-for (j=l;j<=h-1;j++){
-	if (array[j]<=x)
-	{
-		i++;
-                swap(&array[i], &array[j]);
-                
-	}
-
-}
-swap(&array[i+1], &array[h]);
-return (i+1);
-
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 
-
-void quickSort(int array[], int l, int h)
+/* This function is same in both iterative and recursive*/
+int partition(int arr[], int l, int h)
 {
-	int stack[h-l+1];
-	int tos=-1;
+    int x = arr[h];
+    int i = (l - 1);
 
-	stack[++tos]=l;
-	stack[++tos]=h;
-
-	while(tos>=0)
-	{
-		h=stack[tos--];
-                l=stack[tos--];
-		int p=partition(array, l, h);
-                
-		if ((p-1)>l)
-		{
-			stack[++tos]=l;
-			stack[++tos]=p-1;
-
-		}
-
-		if((p+1)<h)
-		{
-			stack[++tos]=p+1;
-			stack[++tos]=h;
-		}
-
-	}
-
+    for (int j = l; j <= h - 1; j++) {
+        if (arr[j] <= x) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[h]);
+    return (i + 1);
 }
 
-void swap(int *a, int *b)
+/* A[] --> Array to be sorted, 
+   l  --> Starting index, 
+   h  --> Ending index */
+void quickSortIterative(int arr[], int l, int h)
 {
-	int tmp;
-	tmp=*a;
-	*a=*b;
-	*b=tmp;
+    // Create an auxiliary stack
+    int stack[h - l + 1];
+
+    // initialize top of stack
+    int top = h-l+1;
+    int tos=h-l+1;
+
+    // push initial values of l and h to stack
+    stack[--top] = l;
+    stack[--top] = h;
+
+    // Keep popping from stack while is not empty
+    while (top < tos) {
+        // Pop h and l
+        h = stack[top++];
+        l = stack[top++];
+
+        // Set pivot element at its correct position
+        // in sorted array
+        int p = partition(arr, l, h);
+
+        // If there are elements on left side of pivot,
+        // then push left side to stack
+        if (p - 1 > l) {
+            stack[--top] = l;
+            stack[--top] = p-1;
+        }
+
+        // If there are elements on right side of pivot,
+        // then push right side to stack
+        if (p + 1 < h) {
+            stack[--top] = p+1;
+            stack[--top] =h;
+        }
+    }
 }
 
-void print(int array[], int size)
+// A utility function to print contents of arr
+void printArr(int arr[], int n)
 {
-	int i;
-	for(i=0;i<size;i++)
-	printf("%d ", array[i]);
-		printf("\n");
+    int i;
+    for (i = 0; i < n; ++i)
+        printf("%d ", arr[i]);
 }
 
+// Driver program to test above functions
 int main()
 {
-	int size=8;
-	int l=0;
-	int h=size-1;
-	int array[8]={10, 20, 80, 30,100, 90, 15, 40};
-        quickSort(array,l,h);
-	print(array, size);
-	return 0;
-
+    int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 };
+    int n = sizeof(arr) / sizeof(*arr);
+    quickSortIterative(arr, 0, n - 1);
+    printArr(arr, n);
+    return 0;
 }
-
-
