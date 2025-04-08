1924eb995b53949ae51807c3eedc052d944257ab
ubhowmik <46545523+ubhowmik@users.noreply.github.com>
2025-04-08 10:45:44 -0500

    Add files via upload

---
+++ lec20/operator_val.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+class Point{
+    private: 
+        int x,y;
+    public:
+        Point(int _x=0, int _y=0){x=_x; y=_y;}
+        
+        void ShowPosition(){cout<<x<<", "<<y<<endl;}
+       
+        void operator+(int val){
+            cout<<"operator+(int)"<<endl;
+            x = x + val;
+            y = y + val;
+        }
+        
+
+/*
+        Point operator+(int val){
+            cout<<"operator+(int)"<<endl;
+            Point temp(x+val, y+val);
+            return temp;
+        }
+*/
+};
+int main(){
+    Point p1(1,2);
+    p1+10;
+    p1.ShowPosition();
+    //Point p2 = p1+10.5;
+    //p2.ShowPosition();
+}
